18c4d168f18aa6574e9e0f7900ecec2cb660454d
robeld <robeld@stanford.edu>
2019-03-02 14:06:01 -0800

    comment in compression_test.py

--- compression_test.py
+++ compression_test.py
@@ -15,7 +15,8 @@ from ernie import QuantizedLayer, layer_check, quantize
 
 '''can't upload file to github, replace with own pretrained file path
 Pretrained model at : http://opennmt.net/Models-py/, under "English->German (WMT)", under the Model column.
-The .pt is the state_dict, not sure about .model at the moment.'''
+The .pt is the state_dict, not sure about .model at the moment.
+TODO - can we get original args of the transformer to load the state_dict from .pt'''
 pretrained_file_path = "../pretrained_transformer/transformer-ende-wmt-pyOnmt/averaged-10-epoch.pt"
 
 pretrained_model = torch.load(pretrained_file_path) # TODO - specify GPU for non-local run
