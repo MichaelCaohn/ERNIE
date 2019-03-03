1bfb2d0908f110d6e51c7ef4195ee9e31e56c9b2
Robin Cheong <robincheong@gmail.com>
2019-03-03 15:07:13 -0800

    updated scripts for downloading data, pretrained model, evaluation

--- download_data.sh
+++ download_data.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
+wget https://s3.amazonaws.com/opennmt-trainingdata/wmt_ende_sp.tar.gz
+tar xvzf wmt_ende_sp.tar.gz
 
-# download all training data
-#wget http://www.statmt.org/wmt13/training-parallel-europarl-v7.tgz
-#wget http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz
-#wget http://www.statmt.org/wmt14/training-parallel-nc-v9.tgz
+mv train.en train.de test.en test.de valid.en valid.de data
 
-# download dev / test sets
-#wget http://www.statmt.org/wmt14/dev.tgz
-#wget http://www.statmt.org/wmt14/test-full.tgz
+python preprocess.py -train_src data/train.en -train_tgt data/train.de -valid_src data/valid.en -valid_tgt data/valid.de -save_data data/wmt14 --vocab_size=32000
 
-# make temp dir and final data dir
-mkdir temp
-mkdir wmt14-en-de
-
-# unpack all
-tar -xvzf training-parallel-europarl-v7.tgz -C temp
-tar -xvzf training-parallel-commoncrawl.tgz -C temp
-tar -xvzf training-parallel-nc-v9.tgz -C temp
-tar -xvzf dev.tgz -C temp
-tar -xvzf test-full.tgz -C temp
-
-# combine en-de files and copy to final data dir
-cat temp/commoncrawl.de-en.en temp/training/europarl-v7.de-en.en temp/training/news-commentary-v9.de-en.en > wmt14-en-de/train.en
-cat temp/commoncrawl.de-en.de temp/training/europarl-v7.de-en.de temp/training/news-commentary-v9.de-en.de > wmt14-en-de/train.de
-
-cp temp/dev/newstest2013.en wmt14-en-de/val.en
-cp temp/dev/newstest2013.de wmt14-en-de/val.de
-
-cp temp/test-full/newstest2014-deen-src.en.sgm wmt14-en-de/test.en
-cp temp/test-full/newstest2014-deen-ref.de.sgm wmt14-en-de/test.de
-
+wget https://s3.amazonaws.com/opennmt-models/transformer-ende-wmt-pyOnmt.tar.gz
+tar xvzf transformer-ende-wmt-pyOnmt.tar.gz
