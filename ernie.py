678b9ce6e8b7ebd36ee6169493ddec77a90f2e12
robeld <robeld@stanford.edu>
2019-03-16 16:01:10 -0700

    iterative pruning

--- ernie.py
+++ ernie.py
@@ -215,21 +215,24 @@ class BinarizedLayer(nn.Module):
         return out
 
 class PrunedLayer(nn.Module):
-    def __init__(self, layer, prop=0.1):
+    def __init__(self, layer, prop=0.1, num_iters=10000):
         """ Implements class-uniform magnitude pruning, ie, prunes x% from the layer passed in
         prop - proportion to prune (eg 0.1 = 10% pruning)
         """
         super(PrunedLayer, self).__init__()
+        self.prop = prop/10
         self.weight = layer.weight
-        self.mask = self.prune(layer.weight, prop)
+        self.mask = self.prune(layer.weight, self.prop)
         self.bias = layer.bias
+        self.counter = 0
 
     def prune(self, params, prop):
         # Technically may prune more than prop if there are multiple of the same weight
         k = int(prop*params.numel())
         shape = params.shape
         absv = params.abs()
-        tk, idxs = torch.topk(absv.flatten(), k, largest=False, sorted=True)
+        topk_tensor = absv.flatten().nonzero()
+        tk, idxs = torch.topk(topk_tensor, k, largest=False, sorted=True)
         threshold = tk[tk.numel()-1].item()
         ones = torch.ones(shape)
         zeros = torch.zeros(shape)
@@ -245,9 +248,12 @@ class PrunedLayer(nn.Module):
         print("type of mask tensor: ", type(self.mask))
         print("mask tensor on cuda: ", self.mask.is_cuda)
         assert(False)'''
+        self.counter += 1
         w = self.weight * self.mask
         bias = self.bias
         out = F.linear(input_, w, bias=bias)
+        if self.counter % 1001:
+            self.mask = self.prune(self.weight, self.prop)
         return out
 def layer_check(model, numLin):
     """ Checks that there are no linear layers in the quantized model, and checks that the number of 
@@ -305,7 +311,7 @@ def quantize(model, num_centroids, error_checking=False, fast=False):
         
     return model
 
-def pruning(model, proportion=0.05, full_model=True):
+def pruning(model, proportion=0.6, full_model=True):
     for name, layer in model.named_children():
         if type(layer) == nn.Linear:
             print(name)
