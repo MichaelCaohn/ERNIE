47cb39d8edb17b54c0ccfa58e1f03741c66071c0
Robin Cheong <robincheong@gmail.com>
2019-03-18 20:42:08 -0700

    updated eval script

--- evaluate.sh
+++ evaluate.sh
@@ -2,7 +2,8 @@
 wget -nc https://raw.githubusercontent.com/OpenNMT/OpenNMT-tf/master/third_party/multi-bleu-detok.perl
 
 output=$1
+test_fn=$2
 out_detok=$output"_detok"
 echo $out_detok
 ./sentencepiece-master/build/src/spm_decode --model sentencepiece.model --input_format piece < $output > $out_detok
-perl multi-bleu-detok.perl val.txt < $out_detok
+perl multi-bleu-detok.perl $2 < $out_detok
